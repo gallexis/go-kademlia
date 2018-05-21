81daa0393377289c09f5fed0dc9a2f61fb03d385
ignaceagbogba <ignaceagbogba@gmail.com>
2018-05-21 16:12:51 +0200

    delete f1 function

--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "crypto/sha1"
-    "io"
     "math/rand"
     "time"
 )
@@ -13,22 +9,6 @@ func init(){
     rand.Seed(time.Now().UTC().UnixNano())
 }
 
-func f1() {
-    f, err := os.Open("file.txt")
-    if err != nil {
-        fmt.Print(err)
-    }
-    defer f.Close()
-
-    h2 := sha1.New()
-
-    if _, err := io.Copy(h2, f); err != nil {
-        fmt.Print(err)
-    }
-
-    //h2.Write([]byte("192.168.1.4"))
-    fmt.Printf("  %x", h2.Sum(nil) )
-}
 
 func main() {
     rt := InitRoutingTable(160, 20)
